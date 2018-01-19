f832bf4a0f0e323eb96bc57667d45a26cb523dee
Avi Flax <avi.flax@fundingcircle.com>
2018-01-19 13:26:39 -0500

    Add new func cpcb to repl

--- src/minimayaml/repl.clj
+++ src/minimayaml/repl.clj
@@ -1,7 +1,12 @@
 (ns minimayaml.repl
   "Some useful funcs for using Minimayaml from the REPL."
-  (:require [minimayaml.core :as c]
+  (:require [clojure.repl :as cr]
+            [clojure.core.async :as ca :refer [chan go-loop offer! poll!]]
+            [clojure.string :as str :refer [includes?]]
+            [minimayaml.core :as c]
             [minimayaml.clipboard :as cb]))
+
+(def stop-chan (chan 1))
 
 (defn pcb
   "Process Clipboard — process the contents of the clipboard and write the results back to the
@@ -16,3 +21,32 @@
           c/process-file
           cb/spit)
       (throw (RuntimeException. "Not a Structurizr diagram.")))))
+
+(defn cpcb
+  "Continuously Process Clipboard — call pcb every second. Stop the routine by calling stop.
+   TODO: there’s a huge chance for a race condition here — need to compare clipboard contents
+   with prior output and only process if it’s changed.
+   "
+  
+  []
+  (go-loop []
+    (Thread/sleep 1000)
+    (try
+      (pcb)
+      (print "🎉 ")
+      (flush)
+      (catch Exception err
+        (print (-> err class .getSimpleName) "")
+        (flush)))
+    (when-not (poll! stop-chan)
+      (recur)))
+  nil)
+
+(defn stop
+  "Stop the goroutine started by cpcb"
+  []
+  (offer! stop-chan true))
+
+(cr/doc pcb)
+(cr/doc cpcb)
+(cr/doc stop)
