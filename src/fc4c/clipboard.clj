688f10b19bf96710567ea778a2f690d4334f6b59
Avi Flax <avi.flax@fundingcircle.com>
2018-03-12 17:54:22 -0400

    clipboard: replace get fn with clipboard var
    I just happened to be in this file and I noticed that this didn’t need
    to be a function and that the code would be more readable and more
    efficient if this was just a value in a var rather than a function.
    
    I’m not 100% sure if this is idiomatic, but I like it.

--- src/fc4c/clipboard.clj
+++ src/fc4c/clipboard.clj
@@ -1,18 +1,17 @@
 (ns fc4c.clipboard
   "A few useful funcs lifted from https://gist.github.com/Folcon/1167903"
-  (:refer-clojure :exclude [get slurp spit]))
+  (:refer-clojure :exclude [slurp spit]))
 
 ; Suppress the Java icon from popping up and grabbing focus on MacOS.
 ; Found in a comment to this answer: https://stackoverflow.com/a/17544259/7012
 (System/setProperty "apple.awt.UIElement" "true")
 
-(defn get []
-  (.getSystemClipboard (java.awt.Toolkit/getDefaultToolkit)))
+(def clipboard (.getSystemClipboard (java.awt.Toolkit/getDefaultToolkit)))
 
 (defn slurp []
   (try
-    (.getTransferData (.getContents (get) nil) (java.awt.datatransfer.DataFlavor/stringFlavor))
+    (.getTransferData (.getContents clipboard nil) (java.awt.datatransfer.DataFlavor/stringFlavor))
     (catch java.lang.NullPointerException e nil)))
 
 (defn spit [text]
-  (.setContents (get) (java.awt.datatransfer.StringSelection. text) nil))
+  (.setContents clipboard (java.awt.datatransfer.StringSelection. text) nil))
