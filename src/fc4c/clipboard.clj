ff35dcbc30c8bb7d02ad5596df4dd280c1162f50
Avi Flax <avi.flax@fundingcircle.com>
2018-03-12 18:14:01 -0400

    clipboard: check content type before reading
    I noticed occasional errors when something other than a string was in my
    clipboard, for example the stacktrace in #12.
    
    This might in fact mostly address #12, but looking at the code in repl/wcb it looks like to *truly* address #12 the entire body of the go-loop needs to be wrapped in a try/catch.

--- src/fc4c/clipboard.clj
+++ src/fc4c/clipboard.clj
@@ -1,17 +1,26 @@
 (ns fc4c.clipboard
   "A few useful funcs lifted from https://gist.github.com/Folcon/1167903"
+  (:import [java.awt Toolkit]
+           [java.awt.datatransfer DataFlavor StringSelection])
   (:refer-clojure :exclude [slurp spit]))
 
 ; Suppress the Java icon from popping up and grabbing focus on MacOS.
 ; Found in a comment to this answer: https://stackoverflow.com/a/17544259/7012
 (System/setProperty "apple.awt.UIElement" "true")
 
-(def clipboard (.getSystemClipboard (java.awt.Toolkit/getDefaultToolkit)))
+(def clipboard (.getSystemClipboard (Toolkit/getDefaultToolkit)))
+
+(def string-flavor (DataFlavor/stringFlavor))
 
 (defn slurp []
   (try
-    (.getTransferData (.getContents clipboard nil) (java.awt.datatransfer.DataFlavor/stringFlavor))
+    ;; We’re gonna check twice for the contents being a string because of
+    ;; possible race conditions.
+    (when (.isDataFlavorAvailable clipboard string-flavor)
+      (let [transferable (.getContents clipboard nil)]
+        (when (.isDataFlavorSupported transferable string-flavor)
+          (.getTransferData transferable string-flavor))))
     (catch java.lang.NullPointerException e nil)))
 
 (defn spit [text]
-  (.setContents clipboard (java.awt.datatransfer.StringSelection. text) nil))
+  (.setContents clipboard (StringSelection. text) nil))
