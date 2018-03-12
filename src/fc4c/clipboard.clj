4a9efb9c402cf2e298def0f7bebfb5b87215efb0
Avi Flax <avi.flax@fundingcircle.com>
2018-03-12 17:53:02 -0400

    Suppress the Java icon from popping up and grabbing focus on MacOS
    This was just annoying.

--- src/fc4c/clipboard.clj
+++ src/fc4c/clipboard.clj
@@ -2,6 +2,10 @@
   "A few useful funcs lifted from https://gist.github.com/Folcon/1167903"
   (:refer-clojure :exclude [get slurp spit]))
 
+; Suppress the Java icon from popping up and grabbing focus on MacOS.
+; Found in a comment to this answer: https://stackoverflow.com/a/17544259/7012
+(System/setProperty "apple.awt.UIElement" "true")
+
 (defn get []
   (.getSystemClipboard (java.awt.Toolkit/getDefaultToolkit)))
 
