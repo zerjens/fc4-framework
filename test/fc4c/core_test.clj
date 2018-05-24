9aadb245a480b278a8ef172be652953e60eadc18
Avi Flax <avi.flax@fundingcircle.com>
2018-05-24 10:30:17 -0400

    Revert "Temporarily break the tests to check if CI builds fail if the tests fail"
    It worked!
    
    840af3b4257caec9376c828dc335e591b845dda6

--- test/fc4c/core_test.clj
+++ test/fc4c/core_test.clj
@@ -3,9 +3,6 @@
             [clojure.test :refer [deftest]]
             [fc4c.test-utils :refer [check]]))
 
-;; temporarily break the tests to ensure that CI builds will fail when the tests fail
-(deftest whatever (clojure.test/is (= 1 2)))
-
 (deftest blank-nil-or-empty? (check `rc/blank-nil-or-empty?))
 (deftest parse-coords (check `rc/parse-coords))
 (deftest round-to-closest (check `rc/round-to-closest))
