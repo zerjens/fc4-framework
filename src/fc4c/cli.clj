4820cbade229390ae6012f637c4b1eaa63c5646f
Avi Flax <avi.flax@fundingcircle.com>
2018-06-18 14:09:49 -0400

    Reorganize namespaces
    This is preparatory refactoring for adding a new feature set wherein
    FC4C has its own data model comprising a model, a set of styles, and a
    set of views.
    
    Working on that helped me realize that the functionality in fc4c.core
    made sense to be there (in that namespace with that name) when FC4C was
    basically just a tool to facilitate working with Structurizr Express (SE)
    diagrams. Now that FC4C has a broader feature set — starting with, for
    now, its own data model, its own YAML formats, and the ability to export
    its own views to SE diagrams — many of these namespaces no longer make
    sense. Since they’re concerned with SE diagrams, this moves them into
    namespaces that are named accordingly.
    
    This also includes a few whitespace removals courtesy of me trying out
    Atom. (I’ve since disabled that feature.)

--- src/fc4c/cli.clj
+++ src/fc4c/cli.clj
@@ -1,11 +1,12 @@
 (ns fc4c.cli
-  (:require [fc4c.clipboard :refer [wcb]]
+  (:require [fc4c.integrations.structurizr.express.clipboard :refer [wcb]]
             [clojure.core.async :as ca :refer [<!!]]))
 
 (defn -main []
   (let [c (wcb)]
-    (println "Now watching clipboard for FC4 diagrams in YAML format. Hit ctrl-c to exit.")
-    
+    (println (str "Now watching clipboard for Structurizr Express diagrams in "
+                  "YAML\format. Hit ctrl-c to exit."))
+
     ;; Block until wcb’s return channel emits something. Which it won’t, so
     ;; effectively block forever, or until the user hits ctrl-c, whichever
     ;; happens first.
