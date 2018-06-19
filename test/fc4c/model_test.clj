f1ca074042615b0bce7603c0b8572d42388fb9dc
Avi Flax <avi.flax@fundingcircle.com>
2018-06-19 13:46:53 -0400

    [WIP++ » fc4c.model] Fix up test code a bit
    Cleanup and also bump up some test numbers for more better quality
    assurance.

--- test/fc4c/model_test.clj
+++ test/fc4c/model_test.clj
@@ -1,14 +1,12 @@
 (ns fc4c.model-test
-  (:refer-clojure :exclude [read])
   (:require [clojure.test :refer [deftest]]
-            [expound.alpha :as expound :refer [explain-results]]
             [fc4c.model :as m]
             [fc4c.test-utils :refer [check]]))
 
-(deftest add-ns (check `m/add-ns 100))
+(deftest add-ns (check `m/add-ns 500))
 (deftest elements-from-file (check `m/elements-from-file 100))
 (deftest fixup-element (check `m/fixup-element 100))
-(deftest get-tags-from-path (check `m/get-tags-from-path 100))
+(deftest get-tags-from-path (check `m/get-tags-from-path))
 (deftest qualify-keys (check `m/qualify-keys 100))
-(deftest to-set-of-keywords (check `m/to-set-of-keywords 100))
+(deftest to-set-of-keywords (check `m/to-set-of-keywords))
 (deftest update-all (check `m/update-all 100))
