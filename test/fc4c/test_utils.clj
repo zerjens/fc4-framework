8305e3271a6f557a47c1929641a1218725b6d934
Avi Flax <avi.flax@fundingcircle.com>
2018-07-10 16:38:28 -0400

    Enhance check fn to support generator overrides.
    This is preparatory refactoring; the next commit will utilize this
    capability.

--- test/fc4c/test_utils.clj
+++ test/fc4c/test_utils.clj
@@ -1,25 +1,33 @@
 (ns fc4c.test-utils
+  "Utility functions to integrate clojure.spec.test/check and expound with
+  clojure.test. Originally inspired by
+  https://gist.github.com/Risto-Stevcev/dc628109abd840c7553de1c5d7d55608"
   (:require [clojure.test             :as t :refer [is]]
             [clojure.spec.alpha       :as s]
             [clojure.spec.test.alpha  :as st]
             [clojure.pprint           :as pprint]
             [expound.alpha            :as expound]))
 
-;; Utility functions to integrate clojure.spec.test/check and expound with
-;; clojure.test, originally inspired by
-;;   https://gist.github.com/Risto-Stevcev/dc628109abd840c7553de1c5d7d55608
-
-(defn opts
-  [num-tests]
-  {:clojure.spec.test.check/opts {:num-tests num-tests}})
+(defn make-opts
+  "Helper to construct options to be passed to st/check.
+  See https://clojure.github.io/spec.alpha/clojure.spec.test.alpha-api.html#clojure.spec.test.alpha/check"
+  [num-tests gen]
+  {:gen gen
+   :clojure.spec.test.check/opts {:num-tests num-tests}})
 
 ;; TODO: maybe this should be a macro; might improve stack traces (which
 ;; currently show the `is` as being in this file, in this fn, because it
 ;; is.)
 (defn check
-  ([sym] (check sym 1000))
+  "Helper fn to integrate clojure.spec.test/check with clojure.test. The third
+  (optional) arg is a map of generator overrides, e.g.
+  `{:foo/bar #(s/gen string?)}`"
+  ([sym]
+    (check sym 1000 {}))
   ([sym num-tests]
-   (let [results (st/check sym (opts num-tests))]
+    (check sym num-tests {}))
+  ([sym num-tests gen]
+   (let [results (st/check sym (make-opts num-tests gen))]
      (is (nil? (-> results first :failure))
          (binding [s/*explain-out* expound/printer]
            (expound/explain-results-str results))))))
