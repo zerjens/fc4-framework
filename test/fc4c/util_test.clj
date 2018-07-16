027897a811223cf3331cb7f7559022c8f388e670
Avi Flax <avi.flax@fundingcircle.com>
2018-07-16 17:29:07 -0400

    Tweak iteration count of some tests
    I just happened to notice that these tests were cranked down lower than
    they needed to be. They’re pretty quick, so the default of 1000
    iterations is fine.

--- test/fc4c/util_test.clj
+++ test/fc4c/util_test.clj
@@ -3,6 +3,6 @@
             [fc4c.util :as u]
             [fc4c.test-utils :refer [check]]))
 
-(deftest add-ns (check `u/add-ns 500))
-(deftest qualify-keys (check `m/qualify-keys 100))
-(deftest update-all (check `m/update-all 100))
+(deftest add-ns       (check `u/add-ns))
+(deftest qualify-keys (check `m/qualify-keys))
+(deftest update-all   (check `m/update-all))
