96d7c3801a742c188736763283e6a4cc3adb0d69
Avi Flax <avi.flax@fundingcircle.com>
2018-10-16 16:50:26 -0400

    [WIP POC Rendering w/Paper.js] Switch renderer to read/write from/to stdin/stdout

---
+++ tool/renderer/README.md
@@ -0,0 +1,9 @@
+# FC4 Renderer
+
+This is a PoC of using Paper.js in Node.js to render FC4 diagrams to PDF and/or PNG files.
+
+## Usage
+
+```shell
+cat examples/test1.json | ./render.js > examples/test1_canvas.pdf
+```
--- tool/renderer/render.js
+++ tool/renderer/render.js
@@ -1,23 +1,28 @@
-const fs = require('fs');
+#!/usr/bin/env node
+
+const {readFile, writeFile} = require('fs');
 const paper = require('paper-jsdom-canvas');
-const path = require('path');
 const {promisify} = require('util');
 
-const readFileAsync = promisify(fs.readFile);
-const writeFileAsync = promisify(fs.writeFile);
+const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
 
-async function renderCanvas(paper_json_input_path, output_path) {
-  const canvas = paper.createCanvas(612, 792, 'pdf');
+function renderCanvas(json_string) {
+  const canvas = paper.createCanvas(500, 400, 'pdf');
   paper.setup(canvas);
-
-  const json_string = await readFileAsync(paper_json_input_path, { encoding: 'utf8' });
-
   paper.project.importJSON(json_string);
   paper.view.update();
-
-  await writeFileAsync(path.resolve(output_path), canvas.toBuffer())
-
-  console.log('Saved!');
+  return canvas.toBuffer()
 }
 
-renderCanvas('examples/test1.json', 'examples/test1_canvas.pdf');
+async function main() {
+  const stdin = 0;
+  const stdout = 1;
+
+  const json_string = await readFileAsync(stdin, { encoding: 'utf8' });
+  const pdf_buffer = renderCanvas(json_string);
+
+  await writeFileAsync(stdout, pdf_buffer);
+}
+
+main();
