d1200297f5c4a53d3227a95206a89cd5e632b795
Avi Flax <avi.flax@fundingcircle.com>
2018-10-16 16:27:00 -0400

    [WIP POC Rendering w/Paper.js] Switch canvas-based example rendering to use async/await 🤩 rather than 😱 callbacks

--- tool/renderer/render.js
+++ tool/renderer/render.js
@@ -1,18 +1,23 @@
-var paper = require('paper-jsdom-canvas');
-var path = require('path');
-var fs = require('fs');
+const fs = require('fs');
+const paper = require('paper-jsdom-canvas');
+const path = require('path');
+const {promisify} = require('util');
 
-var canvas = paper.createCanvas(612, 792, 'pdf');
-paper.setup(canvas);
+const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
 
-fs.readFile('examples/test1.json', { encoding: 'utf8' }, function (err, data) {
-    if (err)
-        throw err;
-    paper.project.importJSON(data);
-    paper.view.update();
-    fs.writeFile(path.resolve('examples/test1_canvas.pdf'), canvas.toBuffer(), function (err) {
-        if (err)
-            throw err;
-        console.log('Saved!');
-    });
-});
+async function renderCanvas(paper_json_input_path, output_path) {
+  const canvas = paper.createCanvas(612, 792, 'pdf');
+  paper.setup(canvas);
+
+  const json_string = await readFileAsync(paper_json_input_path, { encoding: 'utf8' });
+
+  paper.project.importJSON(json_string);
+  paper.view.update();
+
+  await writeFileAsync(path.resolve(output_path), canvas.toBuffer())
+
+  console.log('Saved!');
+}
+
+renderCanvas('examples/test1.json', 'examples/test1_canvas.pdf');
