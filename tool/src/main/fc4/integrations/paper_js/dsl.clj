279179715167fa63b309feb331e011e2bbe2b36b
Avi Flax <avi.flax@fundingcircle.com>
2018-10-17 19:13:26 -0400

    [WIP POC Rendering w/Paper.js] Progress on rendering from Clojure
    including:
    
    * The Paper.js DSL in Clojure
    * Converting an FC4 view to a Paper project data structure
    * Passing the Paper project data structure as JSON to the Node renderer
      as a sub-process

--- tool/src/main/fc4/integrations/paper_js/dsl.clj
+++ tool/src/main/fc4/integrations/paper_js/dsl.clj
@@ -1,27 +1,63 @@
 (ns fc4.integrations.paper-js.dsl)
 
+(defn project
+  [first-child & more-children]
+  (concat [first-child] more-children))
+
 (defn layer
   [first-child & more-children]
   [:Layer {:children (concat [first-child] more-children)}])
 
 (defn group
-  [{:keys [name matrix]} first-child & more-children]
-  [:Group (merge {:children (concat [first-child] more-children)}
-                 (when name {:name name})
-                 (when matrix {:matrix matrix}))])
+  ([{:keys [name matrix]} children]
+   [:Group (merge {:children children}
+                  (when name {:name name})
+                  (when matrix {:matrix matrix}))]))
 
 (defn path
   [m]
   [:Path m])
 
+(defn shape
+  ([m]
+   [:Shape m])
+  ([type m]
+   [:Shape (merge m {:type type})]))
+
+(defn circle
+  [center radius m]
+  (shape :circle (merge m {:center center
+                           :radius radius})))
+
+(defn ellipse
+  [x y width height m]
+  (shape :ellipse (merge m {:point [x y]
+                            :size  [width height]})))
+
+(defn rect
+  [x y width height m]
+  (shape :rectangle (merge m {:point [x y]
+                              :size  [width height]})))
+
+(defn text
+  ([content x y]
+   (text content x y nil))
+  ([content x y m]
+   [:Text (merge m {:content content
+                    :point [x y]})]))
+
 (defn color
   [red green blue]
   ["rgb" red green blue])
 
-(defn shape [TBD] TBD)
+(def representations
+  {:system {:width 100
+            :height 80}})
 
 (defn system
-  [sys]
-  (shape )
-  
-  )
+  [name description x y]
+  (let [{:keys [width height]} (:system representations)]
+    (group
+      {:name name}
+      [(rect x y width height {:strokeColor (color 1 0 0)})
+       (text name x y)])))
--- tool/src/main/fc4/integrations/paper_js/render.clj
+++ tool/src/main/fc4/integrations/paper_js/render.clj
@@ -1,34 +1,56 @@
 (ns fc4.integrations.paper-js.render
-  (:require [fc4.integrations.paper-js.dsl :refer [layer group system]]
+  (:require [clojure.data.json :as json]
+            [clojure.java.shell :refer [sh]]
+            [fc4.integrations.paper-js.dsl :refer [project layer group system]]
             [fc4.model :as m]
             [fc4.view :as v]))
 
 (defn get-sys [model sys-name]
   (get-in model [::m/systems sys-name]))
 
+;;;;; START TEMP
+
+(def coord-pattern-base "(\\d{1,4}), ?(\\d{1,4})")
+(def coord-pattern (re-pattern (str "^" coord-pattern-base "$")))
+(defn parse-coords [s]
+  (some->> s
+           (re-find coord-pattern)
+           (drop 1)
+           (map #(Integer/parseInt %))))
+
+;;;;; END TEMP
+
 (defn view->shapes
   [view model]
-  (for [{:keys [::v/name ::v/coord-string]}
-        (get-in view [::v/positions ::v/other-systems])]
-    ;;;;;;;;;;;;; TODO: finish this!
-    (system (get-sys model name) coord-string)
-  )
-  )
+  (for [[sys-name coord-string]
+        (get-in view [::v/positions ::v/other-systems])
+        :let [sys (get-sys model sys-name)
+              {:keys [::m/name ::m/description]} sys
+              [x y] (parse-coords coord-string)]]
+    (system name description x y)))
 
 (defn view->paper
   "Converts a view on a model to the paper.js data format, effectively rendering
    the view as data. Pure."
   [view model]
   (let [shapes (view->shapes view model)]
-    (layer (group {:name "svg"}
-                  (group nil shapes)))
-  )
+    (project
+      (layer
+        (group nil shapes)))))
 
 (defn paper->pdf
   "Renders a paper.js data structure into a PDF, returning the PDF bytearray.
-  Not pure; spawns a child process to perform the rendering."
+  Not pure; spawns a child process to perform the rendering. If the child
+  process exits with an error code, an exception will be thrown."
   [paper]
-  )
+  (let [json-str (json/write-str paper)
+        result (sh "renderer/render.js"
+                   :in json-str
+                   :out-enc :bytes)
+        {:keys [exit out err]} result]
+    (if (zero? exit)
+        out
+        (throw (Exception. err)))))
 
 (defn render
   "Renders an FC4 view on an FC4 model, returning a PDF bytearray. Not pure;
@@ -36,3 +58,19 @@
   [view model]
   (-> (view->paper view model)
       (paper->pdf)))
+
+(comment
+  (use 'fc4.io)
+  (use 'clojure.java.io)
+  (use :reload 'fc4.integrations.paper-js.dsl)
+  (use :reload 'fc4.integrations.paper-js.render)
+  (def m (read-model "test/data/model (valid)"))
+  (def v (read-view "test/data/views/middle (valid).yaml"))
+  (view->shapes v m)
+  (view->paper v m)
+  (require '[clojure.data.json :as j])
+  (j/pprint (view->paper v m))
+  (def pdfb (render v m))
+  (with-open [out (output-stream (file "/tmp/fc4-paper.pdf"))]
+     (.write out pdfb))
+)
