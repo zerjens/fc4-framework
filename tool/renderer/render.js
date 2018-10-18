9528caabc721108fafba539c9300ed1cba68152d
Avi Flax <avi.flax@fundingcircle.com>
2018-10-17 20:26:30 -0400

    [WIP POC Rendering w/Paper.js] Make render.js fully synchronous
    As a CLI utility, it doesn’t need to be async. This makes the code simpler.

--- tool/renderer/render.js
+++ tool/renderer/render.js
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
 
-const {readFile, writeFile} = require('fs');
+const {readFileSync} = require('fs');
 const paper = require('paper-jsdom-canvas');
 const {promisify} = require('util');
-
-const readFileAsync = promisify(readFile);
-const writeFileAsync = promisify(writeFile);
 
 // These values are currently hard-coded for the content of examples/test1.json
 const [canvasWidth, canvasHeight] = [550, 550];
@@ -23,14 +20,14 @@ function renderCanvas(jsonString, file_type) {
   return canvas.toBuffer()
 }
 
-async function main() {
+function main() {
   // TODO: read this from the command-line, defaulting to 'pdf'
   const fileType = 'pdf';
 
   const stdin = 0;
   const stdout = 1;
 
-  const jsonString = await readFileAsync(stdin, { encoding: 'utf8' });
+  const jsonString = readFileSync(stdin, { encoding: 'utf8' });
   const imageBuffer = renderCanvas(jsonString, fileType);
 
   process.stdout.write(imageBuffer);
