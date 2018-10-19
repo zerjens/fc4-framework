519ccc9ec409a80a34b4cbc9b3e4a12da4d0c7ab
Avi Flax <avi.flax@fundingcircle.com>
2018-10-18 22:40:51 -0400

    [tool: automated rendering] WIP++ PoC with Structurizr Express, Node, Puppeteer, headless Chrome

--- tool/renderer/render.js
+++ tool/renderer/render.js
@@ -4,47 +4,93 @@ const dataUriToBuffer = require('data-uri-to-buffer');
 const {writeFileSync} = require('fs')
 const puppeteer = require('puppeteer');
 
-async function render(diagramYaml) {
-  const browser = await puppeteer.launch();
-  console.log('browser launched');
+function next(step) {
+  console.log(step + '...');
+}
 
+async function waitasec(page) {
+  next('pausing');
+  await page.waitFor(1000);
+}
+
+async function render(diagramYaml) {
+  console.log('launching browser...');
+  const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
 
+  async function asec() {
+    next('pausing');
+    await page.waitFor(1000);
+  }
+
+  next('loading SE');
   await page.goto('https://structurizr.com/express', {'waitUntil' : 'networkidle0'});
-  console.log('SE loaded');
 
-  await page.evaluate(()=> {
-      Structurizr.diagram.exportCurrentView(1, true, false, false, false);
-  });
+  await asec();
 
-  console.log('export function called');
+  next('setting YAML and updating diagram');
 
-  console.log('waiting 1 second...');
-  await page.waitFor(1000);
+  await page.evaluate(theYaml => {
+    function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    (async () => {
+      // helps with debugging, screenshots, etc
+      document.getElementById('expressIntroductionModal').style = "display: none;"
+    await sleep(200);
+      document.querySelector('a[href="#yaml"]').click();
+    await sleep(200);
+      const yamlTextArea = document.getElementById('yamlDefinition');
+    await sleep(200);
+      yamlTextArea.value = theYaml;
+    await sleep(200);
+      changes = true;
+    await sleep(200);
+      structurizrExpressToDiagram();
+    })();
+  }, diagramYaml);
+
+  await asec();
+  await page.evaluate(() => Structurizr.diagram.exportCurrentView(1, true, false, false, false));
+  await asec();
 
   const pages = await browser.pages()
   const exportPage = pages[2];
   const exportPageTitle = await exportPage.title()
   console.log('export page opened with title', exportPageTitle);
 
-  console.log('getting image...');
+  await waitasec(page);
+
+  next('getting image');
   const image = await exportPage.$('#exportedContent > img');
-  console.log('got image.');
+
+  await waitasec(page);
 
   console.log('getting image source...');
-  const imageSourceHandle = await image.evaluate('src');
+  const imageSourceHandle = await image.getProperty('src');
   const imageSource = await imageSourceHandle.jsonValue();
-  console.log('image source:', imageSource);
-
   const imageBuffer = dataUriToBuffer(imageSource);
 
-  console.log('got image source?', imageBuffer)
-
-  console.log('closing browser')
-  await browser.close();
+  console.log('closing browser...')
+await browser.close();
 
   return imageBuffer;
 }
 
-const ib = render(null);
-writeFileSync('/tmp/diagram.png', ib);
+async function main() {
+  const theYaml = `---
+elements:
+  -
+    type: 'Software System'
+    name: 'Fuck Off'
+    position: '300,300'
+type: 'System Context'
+scope: 'Fuck Off'
+size: A6_Landscape
+`
+  const ib = await render(theYaml);
+  writeFileSync('/tmp/diagram.png', ib);
+}
+
+main()
