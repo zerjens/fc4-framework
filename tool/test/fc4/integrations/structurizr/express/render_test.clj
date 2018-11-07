f9bb56c2058d4f656e7ecc31875ab10bb9035e57
Avi Flax <avi.flax@fundingcircle.com>
2018-11-07 14:24:50 -0500

    [tool: automated rendering] Attempt to fix render test
    The problem is that Structurizr Express (SE) puts its watermark in a
    random location at a random angle, so I can’t expect its output to be
    exactly identical to any kind of pre-saved “expected” value.
    
    So this attempt uses an approach of blowing out the contrast of both
    images and hoping that that makes them exactly equal.
    
    There’s also some remnants of another approach in here, using a FilteredImageSource with a filter to remove the watermark’s pixels by
    color-matching… but I couldn’t get that to work. Both approaches will
    be removed soon anyway so they’re really just included in the change
    history for historical reference.

--- tool/test/fc4/integrations/structurizr/express/render_test.clj
+++ tool/test/fc4/integrations/structurizr/express/render_test.clj
@@ -1,12 +1,23 @@
+; Suppress the Java icon from popping up and grabbing focus on MacOS.
+; Found in a comment to this answer: https://stackoverflow.com/a/17544259/7012
+; This is at the top of the file, as opposed to the ns form being first as is
+; idiomatic, because one of the mikera.image namespaces triggers that Java icon
+; on load.
+(System/setProperty "apple.awt.UIElement" "true")
+
 (ns fc4.integrations.structurizr.express.render-test
   (:require [fc4.integrations.structurizr.express.render :as r]
             [clojure.java.io                             :as io :refer [file input-stream]]
             [clojure.spec.alpha                          :as s]
-            [clojure.test                                       :refer [deftest testing is]])
-  (:import  [java.io DataInputStream]
-            [java.util Arrays]))
+            [clojure.test                                       :refer [deftest testing is]]
+            [mikera.image.filters                               :refer [contrast]])
+  (:import  [java.awt Color Toolkit]
+            [java.awt.image BufferedImage FilteredImageSource RGBImageFilter]
+            [java.io ByteArrayInputStream ByteArrayOutputStream DataInputStream]
+            [java.util Arrays]
+            [javax.imageio ImageIO]))
 
-(defn file-to-byte-array
+(defn file->bytes
   "Copied from https://stackoverflow.com/a/29640320/7012"
   [^java.io.File file]
   (let [result (byte-array (.length file))]
@@ -14,25 +25,88 @@
       (.readFully in result))
     result))
 
+(def toolkit (Toolkit/getDefaultToolkit))
+(def watermark-color (Color. 0.99 0.99 0.99))
+(def white-rgb (.getRGB Color/white))
+
+(def watermark-filter
+  (proxy
+    [RGBImageFilter]
+    []
+    (filterRGB [_x _y rgb]
+      ; (let [color-vals (-> (Color. rgb) (.getRGBColorComponents nil))]
+        ; (if (every? #(> % 0.8) color-vals)
+          white-rgb
+          ; rgb
+          )))
+          ;))
+
+(defn image->buffered-image [image]
+  (if (instance? BufferedImage image)
+    image
+    (let [buffered-image (BufferedImage. (.getWidth image nil)
+                                         (.getHeight image nil)
+                                         BufferedImage/TYPE_INT_ARGB)]
+      (doto (.createGraphics buffered-image)
+            (.drawImage image 0 0 nil)
+            (.dispose))
+      buffered-image)))
+
+(defn bytes->buffered-image [bytes]
+  (ImageIO/read (ByteArrayInputStream. bytes)))
+
+(defn image->bytes [image]
+  (with-open [baos (ByteArrayOutputStream.)]
+    (ImageIO/write (image->buffered-image image) "png" baos)
+    (.toByteArray baos)))
+
+(defn remove-watermark [img-bytes]
+  (let [in-image (ImageIO/read (input-stream img-bytes))
+        source (FilteredImageSource. (.getSource in-image) watermark-filter)
+        out-image (.createImage toolkit source)]
+    (image->bytes out-image)))
+
+(defn binary-spit [f data]
+  (with-open [out (io/output-stream (file f))]
+    (.write out data)))
+
+(defn adjust-contrast [ratio image-bytes]
+  (-> (bytes->buffered-image image-bytes)
+      ((contrast ratio))
+      (image->bytes)))
+
+(defn temp-png-file [basename] (java.io.File/createTempFile basename ".png"))
+
 (deftest render
   (testing "happy paths"
     (testing "rendering a Structurizr Express file"
-      (let [dir "test/data/structurizr/express/"
+      ;; NB: this approach of blowing out the image contrast and then checking
+      ;; whether the images are then *exactly* identical, byte-for-byte, is not
+      ;; great. For one thing, it removes light lines and fills, and it’d be
+      ;; better to actually check those. So:
+      ;; TODO: compare image *difference* and consider anything 90+% similar to
+      ;; be equivalent.
+      (let [blow-out (partial adjust-contrast 5.0)
+            dir "test/data/structurizr/express/"
             yaml (slurp (str dir "diagram_valid_cleaned.yaml"))
             {:keys [::r/png-bytes ::r/stderr] :as result} (r/render yaml)
-            expected-file (file (str dir "diagram_valid_cleaned.png"))
-            expected-bytes (file-to-byte-array expected-file)]
+
+            actual-bytes (blow-out png-bytes)
+            expected-file (file (str dir "diagram_valid_cleaned_expected.png"))
+            expected-bytes (-> (file->bytes expected-file)
+                               (blow-out))]
 
         (is (s/valid? ::r/result result) (s/explain-str ::r/result result))
 
-        (is (Arrays/equals png-bytes expected-bytes)
-            ;; NB: apparently `is` evaluates this `msg` arg eagerly, so it’s
+        (is (Arrays/equals actual-bytes expected-bytes)
+            ;; NB: below in addition to returning a message we write the actual
+            ;; bytes out to the file system, to help with debugging. But
+            ;; apparently `is` evaluates this `msg` arg eagerly, so it’s
             ;; evaluated even if the assertion is true. This means that even
             ;; when the test passes the “expected” file is written out to the
             ;; filesystem. So TODO: maybe we should do something about this.
-            (let [actual-file-path (str dir "diagram_valid_cleaned_actual.png")]
-              (with-open [out (io/output-stream (file actual-file-path))]
-                (.write out png-bytes))
+            (let [actual-file (temp-png-file "diagram_valid_cleaned_actual.png")]
+              (binary-spit actual-file actual-bytes)
               (str stderr
                    "\nfile with “expected” PNG: " expected-file
-                   "\nactual PNG written to " actual-file-path)))))))
+                   "\nactual PNG written to " (.getPath actual-file))))))))
