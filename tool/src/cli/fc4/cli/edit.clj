9c4e570ecc6159f428006f012cfc0f36e33a8ed5
Avi Flax <avi.flax@fundingcircle.com>
2018-12-11 15:34:04 -0500

    EP05 WIP++ Tweaks

--- tool/src/cli/fc4/cli/edit.clj
+++ tool/src/cli/fc4/cli/edit.clj
@@ -6,6 +6,7 @@
    [clojure.core.async :as ca :refer [<! chan dropping-buffer go]]
    [clojure.java.io :as io]
    [clojure.string :as cstr :refer [ends-with?]]
+   [fc4.cli.util :refer [debug fail verbose]]
    [fc4.integrations.structurizr.express.edit :as se-edit]
    [fc4.io :as fio :refer [process-text-file render-diagram-file yaml-file?]]
    [hawk.core :as hawk])
@@ -26,19 +27,19 @@
        (yaml-file? file)
        (or (not (contains? context file))
            (let [last-processed (get context file)]
-             ; (println "it’s been" (since last-processed) "seconds since" (.getName file) "was last changed...")
+             (debug "it’s been" (since last-processed) "seconds since" (.getName file) "was last changed...")
              (>= (since last-processed) min-secs-between-changes)))))
 
 (defn process-file
   [context ^File file]
-  (println (.getName file))
+  (print (str (.getName file) "... "))
+  (flush)
   (let [result (se-edit/process-file (slurp file))
         ;; TODO: error handling!
         yaml-out (::se-edit/str-processed result)]
     (spit file yaml-out))
-  (print "processed YAML written to file.\nrendering... ")
   (render-diagram-file file)
-  (println "done.\n " (.getName file) "\n")
+  (println "✅")
   ; Update the state value so process-fn-event? will be able to filter out
   ; events that are dispatched in rapid succession that would otherwise result
   ; in infinite loops.
@@ -48,15 +49,26 @@
   [context {:keys [file] :as _event}]
   (process-file context file))
 
-(defn start-watch
+(def current-watch
+  "Useful during development."
+  (atom nil))
+
+(defn start
   [paths]
   (let [watch (hawk/watch! [{:paths   paths
                              :filter  process-fs-event?
                              :handler process-fs-event}])]
-    (println "📣 Now watching for changes to YAML files under specified paths...")
-    watch))
+    (verbose "📣 Now watching for changes to YAML files under specified paths...")
+    (reset! current-watch watch)))
 
-(defn block-on-watch
+(defn stop
+  "Useful during development, when we won’t want to invoke -main because it
+  invokes block."
+  ([] (stop @current-watch))
+  ([watch]
+   (hawk/stop! watch)))
+
+(defn block
   [{:keys [thread]}]
   (.join thread))
 
@@ -68,6 +80,5 @@
   ;; option ASAP.
   [& paths]
   (when (empty? paths)
-    (println "usage: fc4 edit [path ...]")
-    (System/exit 1))
-  (block-on-watch (start-watch paths)))
+    (fail "usage: fc4 edit [path ...]"))
+  (block (start paths)))
