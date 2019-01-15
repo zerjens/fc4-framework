3ff6ad4070d3e21a8a897a25bb759f28960d0373
Avi Flax <avi.flax@fundingcircle.com>
2019-01-15 18:00:22 -0500

    WIP++

--- tool/src/main/fc4/dsl.clj
+++ tool/src/main/fc4/dsl.clj
@@ -1,9 +1,9 @@
 (ns fc4.dsl
   (:require [clj-yaml.core :as yaml]
-            [clojure.set :refer [superset?]]
+            [clojure.set :refer [intersection superset?]]
             [clojure.spec.alpha :as s]
             [clojure.spec.gen.alpha :as gen]
-            [clojure.string :refer [starts-with?]]
+            [clojure.string :refer [includes? join starts-with?]]
             [cognitect.anomalies :as anom]
             [expound.alpha :as expound :refer [expound-str]]
             [fc4.model :as m]
@@ -78,25 +78,25 @@
   :fn   (fn [{{arg :v} :args, ret :ret}]
           (= (first arg) (first ret))))
 
-(defn validate-file-map
+(defn validate-parsed-file
   "Returns either an error message as a string or nil."
-  [file-map]
+  [parsed]
   (cond
-    (s/valid? ::file-map file-map)
+    (s/valid? ::file-map parsed)
     nil
 
-    (fault? file-map)
-    (::anom/message file-map)
+    (fault? parsed)
+    (::anom/message parsed)
 
     :else
-    (expound-str ::file-map file-map)))
+    (expound-str ::file-map parsed)))
 
-(s/fdef validate-file-map
-  :args (s/cat :v (s/alt :valid   ::file-map
-                         :invalid map?))
-  :ret  (s/or :valid   nil?
-              :invalid string?)
-  :fn   (fn [{{arg :v} :args, ret :ret}]
+(s/fdef validate-parsed-file
+  :args (s/cat :parsed (s/alt :valid   ::file-map
+                              :invalid map?))
+  :ret  (s/or                 :valid   nil?
+                              :invalid string?)
+  :fn   (fn [{{arg :parsed} :args, ret :ret}]
           (= (first arg) (first ret))))
 
 (def ^:private dsl-to-model-maps-singular
@@ -119,7 +119,14 @@
   [model file-map]
   (reduce
    (fn [model [src dest]]
-     (update model dest merge (get file-map src {})))
+     (let [src-map (get file-map src {})
+           dest-map (get model dest)
+           keys-in-both (intersection (set (keys src-map))
+                                      (set (keys dest-map)))]
+       (if (seq keys-in-both)
+         (reduced (fault (str "Cannot build model because of duplicate names "
+                              (join "," keys-in-both))))
+         (update model dest merge src-map))))
    model
    dsl-to-model-maps))
 
@@ -142,18 +149,56 @@
 (s/fdef add-file-map
   :args (s/cat :pmodel   ::m/proto-model
                :file-map ::file-map)
-  :ret  ::m/proto-model
-  :fn   (fn [{{:keys [file-map]} :args, ret :ret}]
-          (contains-contents? ret file-map)))
+  :ret  (s/or :success ::m/proto-model
+              :failure ::anom/anomaly)
+  :fn   (fn [{{:keys [pmodel file-map]} :args
+              [ret-tag ret-val]         :ret}]
+          (and
+           ; The :ret spec allows the return value to be either a proto-model
+           ; or a valid anomaly. If a value is passed to it that is actually
+           ; both a valid proto-model *and* a valid anomaly, it will be
+           ; considered valid by the spec, and will be tagged as a :success,
+           ; but only because :success is the first spec in the or. So let’s
+           ; just ensure this doesn’t happen.
+           (not (and (s/valid? ::m/proto-model ret-val)
+                     (s/valid? ::anom/anomaly  ret-val)))
+           (case ret-tag
+             ;; TODO: also validate that ret-val contains the contents of pmodel.
+             :success
+             (contains-contents? ret-val file-map)
+
+             ;; TODO: also validate that the inputs do indeed have duplicate keys
+             :failure
+             (includes? (or (::anom/message ret-val) "") "duplicate names")))))
 
 (defn build-model
   "Accepts a sequence of maps read from model YAML files and combines them into
-  a single model map. Does not validate the result."
+  a single model map. If any name collisions are detected then an anomaly is
+  returned. Does not validate the result."
   [file-maps]
-  (reduce add-file-map (m/empty-model) file-maps))
+  (reduce
+   (fn [model file-map]
+     (let [result (add-file-map model file-map)]
+       (if (fault? result)
+         (reduced result)
+         result)))
+   (m/empty-model)
+   file-maps))
 
 (s/fdef build-model
   :args (s/cat :in (s/coll-of ::file-map))
-  :ret  ::m/proto-model
-  :fn   (fn [{{:keys [in]} :args, out :ret}]
-          (every? #(contains-contents? out %) in)))
+  :ret  (s/or :success ::m/proto-model
+              :failure ::anom/anomaly)
+  :fn   (fn [{{:keys [in]}      :args
+              [ret-tag ret-val] :ret}]
+          (and
+           ; I saw, at least once, a case wherein the return value was  both a
+           ; valid proto-model *and* a valid anomaly. We don’t want this.
+           (not (and (s/valid? ::m/proto-model ret-val)
+                     (s/valid? ::anom/anomaly  ret-val)))
+           (case ret-tag
+             :success
+             (every? #(contains-contents? ret-val %) in)
+
+             :failure
+             (includes? (or (::anom/message ret-val) "") "duplicate names")))))
--- tool/test/fc4/dsl_test.clj
+++ tool/test/fc4/dsl_test.clj
@@ -3,7 +3,10 @@
             [fc4.dsl :as dsl]
             [fc4.test-utils :refer [check]]))
 
-(deftest parse-model-file   (check `dsl/parse-model-file 100))
-(deftest validate-file-map  (check `dsl/validate-file-map 100))
-(deftest add-file-map       (check `dsl/add-file-map 50))
-(deftest build-model        (check `dsl/build-model 10))
+(deftest parse-model-file     (check `dsl/parse-model-file 100))
+(deftest validate-parsed-file (check `dsl/validate-parsed-file 100))
+(deftest add-file-map         (check `dsl/add-file-map 50))
+
+;; 10 is a really low number of test cases, but it takes ~45 seconds on my
+;; laptop. So it might be worth looking into speeding up this test.
+(deftest build-model          (check `dsl/build-model 10))
